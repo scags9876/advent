54081094090f2916b61af9c10b5e7081dd3a7626
John Scaglione <john.scaglione@whitehatsec.com>
2020-12-05 22:01:24 -0800

    day5 part 2

--- 2020/day5/main.go
+++ 2020/day5/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,6 +13,7 @@ const inputFilename = "input.txt"
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -42,4 +44,47 @@ func part1() {
 	}
 
 	fmt.Printf("Part 1: Highest seat ID: %d\n", highestSeatID)
+}
+func part2() {
+	file, err := os.Open(inputFilename)
+	if err != nil {
+		panic(fmt.Errorf("%s file not found", inputFilename))
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	seatIDs := []int{}
+	r := strings.NewReplacer("F", "0", "L", "0", "B", "1", "R", "1")
+
+	for sc.Scan() {
+		pass := sc.Text()
+		seatID, err := strconv.ParseInt(r.Replace(pass), 2, 64)
+		if err != nil {
+			panic(err)
+		}
+		seatIDs = Insert(seatIDs, int(seatID))
+	}
+
+	lastSeatID := 0
+	mySeat := 0
+	for _, seatID := range seatIDs {
+		fmt.Printf("seatID: %d\n", seatID)
+		if lastSeatID != 0 && lastSeatID+1 != seatID {
+			fmt.Printf("Gap in the seats!  lastSeatID: %d, this seatID: %d\n", lastSeatID, seatID)
+			mySeat = lastSeatID+1
+			break
+		}
+		lastSeatID = seatID
+	}
+
+	fmt.Printf("Part 2: My seatID is: %d\n", mySeat)
+}
+
+func Insert(ss []int, s int) []int {
+	i := sort.SearchInts(ss, s)
+	ss = append(ss, 0)
+	copy(ss[i+1:], ss[i:])
+	ss[i] = s
+	return ss
 }
