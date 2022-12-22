61520bef65b5c2a5965a25aa19a2a953d71490ab
John Scaglione <john.scaglione@whitehatsec.com>
2022-12-22 08:36:12 -0800

    Update to template

--- templates/day/main.go
+++ templates/day/main.go
@@ -8,17 +8,20 @@ import (
 
 const inputFilename = "input.txt"
 const testInputFilename = "testinput.txt"
-const expectedTestResultPart1 = 7
+const expectedTestResultPart1 = 15
 const expectedTestResultPart2 = 5
 
 const verbose = true
 
 func main() {
-	testInput := lib.GetInputInts(testInputFilename)
-	input := lib.GetInputInts(inputFilename)
+	testInput := lib.GetInputStrings(testInputFilename)
+	input := lib.GetInputStrings(inputFilename)
 
 	fmt.Print("Part 1:\n")
 	testResult := part1(testInput)
+	if verbose {
+		fmt.Printf("Part 1 testInput result: %d\n", testResult)
+	}
 	if testResult == expectedTestResultPart1 {
 		fmt.Printf("testfile returned expected result! (%d)\n", testResult)
 	} else {
@@ -26,10 +29,13 @@ func main() {
 	}
 
 	result := part1(input)
-	fmt.Printf("result: %d\n", result)
+	fmt.Printf("Part 1 result: %d\n", result)
 
 	fmt.Print("\nPart 2: \n")
 	testResult = part2(testInput)
+	if verbose {
+		fmt.Printf("Part 2 testInput result: %d\n", testResult)
+	}
 	if testResult == expectedTestResultPart2 {
 		fmt.Printf("testfile returned expected result! (%d)\n", testResult)
 	} else {
@@ -40,17 +46,23 @@ func main() {
 	fmt.Printf("Part 2 result: %d\n", result)
 }
 
-func part1(input []int) int {
+func part1(input []string) int {
 	var result int
 	for i := range input {
+		if verbose {
+			fmt.Printf("line %2d\n", i)
+		}
 		result++
 	}
 	return result
 }
 
-func part2(input []int) int {
+func part2(input []string) int {
 	var result int
 	for i := range input {
+		if verbose {
+			fmt.Printf("line %2d\n", i)
+		}
 		result++
 	}
 	return result
